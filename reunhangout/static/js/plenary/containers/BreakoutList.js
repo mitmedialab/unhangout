600b9eac36a188c90028961d8702460ef6e425d8
zpgong <zoepgong@gmail.com>
2016-07-12 17:44:09 -0400

    fixed a typo

--- reunhangout/static/js/plenary/containers/BreakoutList.js
+++ reunhangout/static/js/plenary/containers/BreakoutList.js
@@ -1,5 +1,4 @@
 import React from "react";
-import ReactDOM from "react-dom"
 import {connect} from "react-redux";
 import * as style from "../../../scss/pages/plenary/_breakoutliststyle.scss";
 import * as BS from "react-bootstrap";
