22f07e06a2b8b6702b8e5725388c7f0e25363d0b
Charlie DeTar <cfd@media.mit.edu>
2016-08-07 16:32:06 -0600

    Add dev installer script

---
+++ dev_install.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+PROJECT_NAME=reunhangout
+
+set -e
+
+if ! which python3 > /dev/null 2>&1; then
+    echo "Please install python3 first."
+    exit 1
+fi
+if ! which node > /dev/null 2>&1; then
+    echo "Please install nodejs version 4 or higher."
+    exit 1
+fi
+if ! which virtualenv > /dev/null 2>&1; then
+    echo "Please install virtualenv."
+    exit 1
+fi
+
+NODE_VERSION=`node --version`
+if ! [[ $NODE_VERSION =~ ^v[456]\.[0-9]+\.[0-9]+$ ]]; then
+    echo "Please install nodejs version 4 or higher.  $NODE_VERSION installed."
+    exit 1
+fi
+
+
+DIR=$(cd -P -- "$(dirname -- "$0")" && pwd -P)
+VENV=$DIR/venv
+if [ ! -d "$VENV" ]; then
+    virtualenv --no-site-packages --python "`which python3`" $VENV
+fi
+
+$VENV/bin/pip install -r $DIR/shrinkwrap.txt
+cd "$DIR/$PROJECT_NAME"
+npm install
+npm run build:dev
+
+if [ ! -e "$DIR/$PROJECT_NAME/$PROJECT_NAME/settings.py" ]; then
+    cp "$DIR/$PROJECT_NAME/$PROJECT_NAME/example.settings.py" "$DIR/$PROJECT_NAME/$PROJECT_NAME/settings.py"
+fi
+
+$VENV/bin/python "$DIR/$PROJECT_NAME/manage.py" migrate
+
+echo 'Install successful!'
