1ee18a93d91cc246662e3c8ec6d2c82febe1b385
Charlie DeTar <cfd@media.mit.edu>
2016-09-05 14:29:42 -0600

    Don't override bootstrap in breakout style

--- reunhangout/static/js/breakout/containers/Breakout.js
+++ reunhangout/static/js/breakout/containers/Breakout.js
@@ -8,7 +8,6 @@ import * as style from "../../../scss/pages/breakout/_breakoutstyle.scss"
 
 class Breakout extends React.Component {
   render() {
-    console.log(this.props.breakout)
     if (!this.props.presence || !this.props.presence.channel_name) {
       return this.renderStatusMessage("Loading...");
     }
@@ -21,29 +20,26 @@ class Breakout extends React.Component {
 
     return <div className='breakout-detail'>
       <ConnectionStatus />
-      <BS.Grid fluid>
-        <BS.Row>
-          <BS.Col xs={2}>
-            <div className="breakout-title-container">
-              <span>{this.props.breakout.title}</span>
-            </div>
-            <Presence presence={this.props.presence} auth={this.props.auth} />
-            { this.props.breakoutMessages.length > 0 ?
-                <ul className='breakout-messages'>
-                  {
-                    this.props.breakoutMessages.map((message, i) => {
-                      return <li key={`message-${i}`}>{message}</li>
-                    })
-                  }
-                </ul>
-              : "" }
-          </BS.Col>
-          <BS.Col xs={10}>
-            <iframe src={`https://meet.jit.si/${this.props.breakout.webrtc_id}`}
-              width={800} height={480}></iframe>
-          </BS.Col>
-        </BS.Row>
-      </BS.Grid>
+      <div className='breakout-columns'>
+        <div className='breakout-left-col'>
+          <div className="breakout-title-container">
+            <span>{this.props.breakout.title}</span>
+          </div>
+          <Presence presence={this.props.presence} auth={this.props.auth} />
+          { this.props.breakoutMessages.length > 0 ?
+              <ul className='breakout-messages'>
+                {
+                  this.props.breakoutMessages.map((message, i) => {
+                    return <li key={`message-${i}`}>{message}</li>
+                  })
+                }
+              </ul>
+            : "" }
+        </div>
+        <div className='breakout-right-col'>
+          <iframe src={`https://meet.jit.si/${this.props.breakout.webrtc_id}`} frameBorder={0}></iframe>
+        </div>
+      </div>
     </div>;
   }
 
