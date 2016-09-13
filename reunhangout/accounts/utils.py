24522f76c5d161a356ba03193df2bb9b45525f59
Charlie DeTar <cfd@media.mit.edu>
2016-09-13 09:44:28 -0600

    Include image with

--- reunhangout/accounts/utils.py
+++ reunhangout/accounts/utils.py
@@ -1,12 +1,19 @@
 def serialize_auth_state(user, plenary=None):
     if not user.is_authenticated():
-        return {'auth': {'is_authenticated': False}}
+        return {
+            'auth': {
+                'is_authenticated': False,
+                'username': "Anonymous",
+                "image": User.default_profile_image(),
+            }
+        }
 
     return {
         'auth': {
             'is_authenticated': True,
             'id': user.id,
             'username': user.username,
+            'image': user.get_profile_image(),
             'email': user.email,
             'is_superuser': user.is_superuser,
             'is_admin': user.is_superuser or (plenary and plenary.admins.filter(pk=user.pk).exists()),
