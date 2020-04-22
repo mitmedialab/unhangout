542523f027775a823aec47cc03552e88755c2f0e
Dirk Uys <dirkcuys@gmail.com>
2020-04-22 16:55:27 +0200

    Update entrypoint

--- docker/entry.sh
+++ docker/entry.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 set -e
 
-if [ "$1" = '/opt/app-venv/bin/python' ]; then
+if [ "$1" = '/opt/app-venv/bin/daphne' ]; then
     /opt/app-venv/bin/python /opt/app/manage.py migrate --noinput
     /opt/app-venv/bin/python /opt/app/manage.py collectstatic --noinput
     exec "$@"
