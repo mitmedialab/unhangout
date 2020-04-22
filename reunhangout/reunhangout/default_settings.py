d8b4a39c650a7b4e0889b28cff468a6534f77cf3
Dirk Uys <dirkcuys@gmail.com>
2020-04-22 22:49:36 +0200

    Update default_settings with all required settings for production

--- reunhangout/reunhangout/default_settings.py
+++ reunhangout/reunhangout/default_settings.py
@@ -1,27 +1,26 @@
 """
 Django settings for reunhangout project.
 """
-
 import os
 from datetime import timedelta
 
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-env = lambda key, default: os.environ.get(key, default)
-
+env = lambda key, default=None: os.environ.get(key, default)
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.9/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '^m7!_+vj@cm0du#6k92^q41#&pyadk(mo+gukeisj4)fg8ggfj'
+SECRET_KEY = env('SECRET_KEY', '^m7!_+vj@cm0du#6k92^q41#&pyadk(mo+gukeisj4)fg8ggfj')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = THUMBNAIL_DEBUG = True
+DEBUG = THUMBNAIL_DEBUG = False
 
 ALLOWED_HOSTS = []
-if env('DOMAIN', False):
-    ALLOWED_HOSTS = [env('DOMAIN', '')]
+if env('DOMAIN'):
+    ALLOWED_HOSTS = [env('DOMAIN')]
 
 # Application definition
 
@@ -55,6 +54,7 @@ INSTALLED_APPS = [
     'django.contrib.sites',
     'django.contrib.staticfiles',
 ]
+
 MIGRATION_MODULES = {
     'thumbnail': 'frontend.sorl_migrations',
 }
@@ -146,7 +146,6 @@ AUTH_USER_MODEL = 'accounts.User'
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
-
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'public')
 STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
@@ -183,7 +182,8 @@ CELERYBEAT_SCHEDULE = {
     },
 }
 CELERYBEAT_SCHEDULER = 'django_celery_beat.schedulers:DatabaseScheduler'
-BROKER_URL = "redis://localhost:6379/0"
+# TODO BROKER_URL = "redis://localhost:6379/0"
+BROKER_URL = f'redis://{env("REDIS_HOST", '127.0.0.1')}:{env("REDIS_PORT", 6379)}/0'
 
 SOCIALACCOUNT_PROVIDERS = {
     'google': {
@@ -223,10 +223,16 @@ ALLAUTH_APP_KEYS = {
 
 # API keys for 3rd party services.  Don't put auth API keys here; put them in
 # ALLAUTH_APP_KEYS.
+
+# Instructions for obtaining the youtube api key:
+# https://developers.google.com/youtube/v3/getting-started
+# Enable "YouTube Data API v3".
 PUBLIC_API_KEYS = {
     'youtube': env('YOUTUBE_API_KEY', ''), # Define in settings.py
 }
-PLENARY_SERVER = "http://localhost:8000" # Override in settings.py
+
+# TODO remove this variable
+PLENARY_SERVER = 'https://plenary.unhangout.io'
 BRANDING = {
     'logo': 'assets/unhangout-logo-FULL-simple.svg',
     'logo_png': 'assets/unhangout-logo-FULL-simple.png',
@@ -245,7 +251,11 @@ ETHERPAD_DEFAULT_TEXT = "Welcome to the breakout! Use this space for notes or id
 
 MJML_EXEC_CMD = os.path.join(BASE_DIR, "node_modules", ".bin", "mjml")
 
-MAILGUN_ACTIVE_API_KEY = "..."
+EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
+EMAIL_HOST = env('EMAIL_HOST', '')
+EMAIL_HOST_USER = env('EMAIL_HOST_USER', '')
+EMAIL_HOST_PASSWORD = env('EMAIL_HOST_PASSWORD', '')
+MAILGUN_ACTIVE_API_KEY = env('MAILGUN_ACTIVE_API_KEY', '')
 
 WEBPACK_LOADER = {
     'DEFAULT': {
@@ -254,4 +264,93 @@ WEBPACK_LOADER = {
     }
 }
 
+# prod_settings.py
+# ================
 
+from django.http import UnreadablePostError
+
+def skip_unreadable_post(record):
+    if record.exc_info:
+        exc_type, exc_value = record.exc_info[:2]
+        if isinstance(exc_value, UnreadablePostError):
+            return False
+    return True
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'formatters': {
+        'verbose': {
+            'format': '%(levelname)s %(asctime)s %(module)s %(process)d %(thread)d %(message)s'
+        },
+        'simple': {
+            'format': '%(levelname)s %(message)s'
+        },
+    },
+    'handlers': {
+        'console':{
+            'level': 'DEBUG',
+            'class': 'logging.StreamHandler',
+            'formatter': 'simple'
+        },
+        'log_file':{
+            'level': 'DEBUG',
+            'class': 'logging.handlers.RotatingFileHandler',
+            'filename': os.path.join(os.path.dirname(BASE_DIR), 'logs', 'django.log'),
+            'maxBytes': 16777216, # 16megabytes
+            'formatter': 'verbose'
+        },
+        'mail_admins': {
+            'level': 'ERROR',
+            'class': 'django.utils.log.AdminEmailHandler',
+            'include_html': True,
+            'filters': ['require_debug_false', 'skip_unreadable_posts'],
+        }
+    },
+    'loggers': {
+        'django.request': {
+            'handlers': ['mail_admins', 'log_file', 'console'],
+            'level': 'ERROR',
+            'propagate': True,
+        },
+    },
+    'filters': {
+        'require_debug_false': {
+            '()': 'django.utils.log.RequireDebugFalse'
+        },
+        'skip_unreadable_posts': {
+            '()': 'django.utils.log.CallbackFilter',
+            'callback': skip_unreadable_post
+        }
+    }
+}
+
+CACHES = {
+    'default': {
+        'BACKEND': 'django.core.cache.backends.memcached.PyLibMCCache',
+        'LOCATION': '127.0.0.1:11211',
+    }
+}
+
+
+# ansible settings template
+# =========================
+
+DEFAULT_FROM_EMAIL = SERVER_EMAIL = "Unhangout Team <noreply@{{ django_domain }}>"
+ADMINS = MANAGERS = [("Charlie DeTar", "cfd@media.mit.edu")]
+
+
+ACCOUNT_DEFAULT_HTTP_PROTOCOL = 'https'
+# Add client id's and secrets for social account providers. Changes only take
+# effect when you sync them to the database with `./manage.py sync_auth_providers`.
+ALLAUTH_APP_KEYS['twitter']['client_id'] = env('TWITTER_CLIENT_ID', '')
+ALLAUTH_APP_KEYS['twitter']['secret'] = env('TWITTER_SECRET', '')
+ALLAUTH_APP_KEYS['facebook']['client_id'] = env('FACEBOOK_CLIENT_ID', '')
+ALLAUTH_APP_KEYS['facebook']['secret'] = env('FACEBOOK_SECRET', '')
+ALLAUTH_APP_KEYS['google']['client_id'] = env('GOOGLE_CLIENT_ID', '')
+ALLAUTH_APP_KEYS['google']['secret'] = env('GOOGLE_SECRET', '')
+
+# Add INSTALLED_APPS for any in-use social account providers.
+for name, keys in ALLAUTH_APP_KEYS.items():
+    if keys['client_id']:
+        INSTALLED_APPS.append('allauth.socialaccount.providers.%s' % name)
