02c10ceaa646250ebb251ad52b8fe924a78f8fa3
Dirk Uys <dirkcuys@gmail.com>
2020-04-23 23:30:21 +0200

    chown celery dir

--- docker/entry.sh
+++ docker/entry.sh
@@ -3,8 +3,13 @@ set -e
 
 if [ "$1" = '/opt/app-venv/bin/daphne' ]; then
     /opt/app-venv/bin/python /opt/app/manage.py migrate --noinput
+    /opt/app-venv/bin/python /opt/app/manage.py sync_auth_providers --noinput
     /opt/app-venv/bin/python /opt/app/manage.py collectstatic --noinput
     exec "$@"
+fi
+
+if [ "$1" = '/opt/app-venv/bin/celery' ]; then
+    chown -R celery:celery /var/lib/celery
 fi
 
 exec "$@"
