73815aaed374decc7814f271e6ec7ccd4c50a404
Dirk Uys <dirkcuys@gmail.com>
2020-05-08 12:52:17 +0200

    Add updated e2e test

--- reunhangout/plenaries/tests/test_frontend.py
+++ reunhangout/plenaries/tests/test_frontend.py
@@ -2,19 +2,27 @@ import pytest
 from channels.testing import ChannelsLiveServerTestCase
 from selenium.webdriver.common.by import By
 from selenium import webdriver
+from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
 
 from django.contrib.auth import get_user_model
+
+import socket
 
 User = get_user_model()
 
 
 class SomeLiveTests(ChannelsLiveServerTestCase):
     serve_static = True
+    host = '0.0.0.0'
 
     @classmethod
     def setUpClass(cls):
         super().setUpClass()
-        cls.driver = webdriver.Firefox()
+        cls.host = socket.gethostbyname(socket.gethostname())
+        cls.driver = webdriver.Remote(
+            command_executor='http://selenium:4444/wd/hub',
+            desired_capabilities=DesiredCapabilities.CHROME
+        )
         cls.driver.implicitly_wait(10)
 
     @classmethod
@@ -30,14 +38,20 @@ class SomeLiveTests(ChannelsLiveServerTestCase):
             email='user@example.com',
         receive_wrapup_emails=True)
 
+    def _url(self, path):
+        url = self.live_server_url
+        print(url)
+        return url + path
+        #return url.replace('localhost', 'test') + path
+
     def test_add_plenary(self):
         #TODO login user
-        self.driver.get(self.live_server_url + '/accounts/login/')
+        self.driver.get(self._url('/accounts/login/'))
         username_input = self.driver.find_element_by_name("login")
         username_input.send_keys('user')
         password_input = self.driver.find_element_by_name("password")
         password_input.send_keys('password')
         self.driver.find_element(By.CSS_SELECTOR, "button[type=submit]").click()
 
-        self.driver.get(self.live_server_url + '/events/add/')
+        self.driver.get(self._url('/events/add/'))
         assert 'Add an Event' in self.driver.title
